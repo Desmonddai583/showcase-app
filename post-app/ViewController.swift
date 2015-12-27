59f17668b845e8b03aa98c03c54761719a6b3bed
desmonddai583 <desmonddai58@gmail.com>
2015-12-27 16:58:48 +0800

    Initial Commit

---
+++ post-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  post-app
+//
+//  Created by Desmond Dai on 27/12/2015.
+//  Copyright © 2015年 Desmond Dai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
